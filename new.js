5caa795cece1aee2c789f8c05c36782362699bbd
Matheka Lawrence <90851966+mathekalawrence@users.noreply.github.com>
2025-09-04 17:28:23 +0300

    Add files via upload

---
+++ new.js
@@ -0,0 +1,15 @@
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i=0; i<count; i=i+1){
+	rows.push(character.repeat(i));
+}
+
+let result=""
+ for (const row of rows){
+ 	result=result+row+ "\n";
+ }
+
+ console.log(result);
